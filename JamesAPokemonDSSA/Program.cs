31d116cfbd21f443d40322c20ea871438a08a2dc
James <jamesashby1@hotmail.co.uk>
2021-04-04 03:04:21 +0100

    edited it again

--- JamesAPokemonDSSA/Program.cs
+++ JamesAPokemonDSSA/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace JamesAPokemonDSSA
 {
@@ -29,13 +28,7 @@ namespace JamesAPokemonDSSA
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-                    config.AddAzureKeyVault(
-                    keyVaultEndpoint,
-                    new DefaultAzureCredential());
-                }).ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
